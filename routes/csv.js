41e2df9d94151356ea2d7134d9061a2e2e8b3661
Daniel Ramos <danielramosacosta@hotmail.com>
2016-04-23 18:37:36 +0100

    Separando el nombre, y * es para obtener toda la colección

--- routes/csv.js
+++ routes/csv.js
@@ -1,13 +1,24 @@
-/*jslint browser: true, this: true*/
-/*global
-    console 
-*/
-'use strict';
-const express = require('express'),
-    calc = require('../src/js/csv');
-const router = express.Router();
-console.log(calc);
-router.get('/', (req, res) => {
-    res.json(calc(req.query.input));
-});
-module.exports = router;
+(() => {
+
+    'use strict';
+    const express = require('express'),
+        calc = require('../src/js/csv');
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        res.json(calc(req.query.input));
+    });
+
+    router.get('/:fichero', (req, res) => {
+        if (req.params.fichero === '*') {
+            console.log(`Hay que devolver un array con todos los ficheros`);
+            res.json([1, 2, 3]);
+        } else {
+            console.log(`Hay que buscar en la base de datos: ${req.params.fichero}`);
+            res.json({filename: 'input.txt', data: "\"producto\",           \"precio\"\r\n\"camisa\",             \"" +
+                "4,3\"\r\n\"libro de O\\\"Reilly\", \"7,2\"\r\n"});
+        }
+    });
+
+    module.exports = router;
+})();
