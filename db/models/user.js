9933e86558e39c2eb0a215405955e8f1f08fac61
Daniel Ramos <danielramosacosta@hotmail.com>
2016-05-03 04:01:52 +0100

    Cambiado el schema con condicion de integridad, añadido data para que no estalle

--- db/models/user.js
+++ db/models/user.js
@@ -1,10 +1,21 @@
 (() => {
     'use strict';
     const mongoose = require('mongoose');
+    const File = require('./file.js');
+
     const Schema = mongoose.Schema;
     let UserShema = mongoose.Schema({
         username: String,
-        files : [{ type: Schema.Types.ObjectId, ref: 'File' }]
+        files: [{ type: Schema.Types.ObjectId, ref: 'File' }]
     });
+
+    // Si se elimina un usuario, eliminar sus ficheros en cascada
+    UserShema.pre('remove', function(next) {
+        this.files.forEach((it) => {
+            File.remove({_id: it}).exec();
+        });
+        next();
+    });
+
     module.exports = mongoose.model("User", UserShema);
 })();
