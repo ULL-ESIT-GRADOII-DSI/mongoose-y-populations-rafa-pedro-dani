8225247b365a53d505864573a5cade0ce961b733
Daniel Ramos <danielramosacosta@hotmail.com>
2016-05-04 22:49:21 +0100

    Arreglada la relación de usuarios-ficheros, closes #2

--- db/models/file.js
+++ db/models/file.js
@@ -8,11 +8,11 @@
     let FileShema = mongoose.Schema({
         filename: String,
         data: String,
-        owner: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+        owner: { type: Schema.Types.ObjectId, ref: 'User' }
     });
-    
+
     const File = mongoose.model("File", FileShema);
-    
+
     FileShema.pre('save', function(next) {
         console.log("El owener es: " + this.owner)
         File.find({filename: this.filename}, (err, ficheros) => {
@@ -31,7 +31,7 @@
                         console.log('hubo errores al buscar el propietario del fichero');
                         return err;
                     }
-                    
+
                     console.log("hay " + ficheros.length + " ficheros en la base de datos");
                     if(ficheros.length > 3) {
                         File.remove({_id: ficheros[0]._id}).exec();
@@ -49,7 +49,7 @@
                             usuario.save((err) => {
                                 if(err) {
                                     console.log('hubo errores al guardar el usuario');
-                                    return err; 
+                                    return err;
                                 }
                                 next();
                             });
@@ -61,8 +61,8 @@
             }
         })
     });
-    
+
     console.log(Object.keys(File).length);
-    
+
     module.exports = mongoose.model("File", FileShema);
 })();
--- db/models/user.js
+++ db/models/user.js
@@ -4,14 +4,14 @@
     const File = require('./file.js');
     console.log("Soy models/user")
     console.log(Object.keys(File).length);
-    
+
 
     const Schema = mongoose.Schema;
     let UserShema = mongoose.Schema({
         username: String,
         files: [{ type: Schema.Types.ObjectId, ref: 'File' }]
     });
-    
+
     // Si se elimina un usuario, eliminar sus ficheros en cascada
     UserShema.pre('remove', function(next) {
         this.files.forEach((it) => {
