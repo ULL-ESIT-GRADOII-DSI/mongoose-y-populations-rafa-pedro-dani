47f850213f597d0afd7a0cbdf696cc10891696fd
alu0100505078 <alu0100505078@ull.edu.es>
2016-05-05 19:19:51 +0000

    Pasado el lint a un par de ficheros

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-/*jslint browser: true, this: true*/
+/* global $ localStorage _*/
 
 ((exports) => {
     'use strict';
@@ -23,11 +23,11 @@
     };
     /*Constante que vuelca el contenido de los ficheros de
     prueba en el text area que tiene identificador original*/
-    const dump = (fileName) => {
+    /*const dump = (fileName) => {
         $.get(fileName, (data) => {
             $('#original').val(data.data);
         });
-    };
+    };*/
 
     const main = () => {
         let input = $('#original').val();
@@ -46,8 +46,7 @@
     };
 
     $(() => {
-        
-        let usuarioactual = undefined;
+        let usuarioactual;
         // If the browser supports localStorage and we have some stored data
         if (window.localStorage && localStorage.original) {
             $('#original').val(localStorage.original);
@@ -63,18 +62,19 @@
         $('#pag_completa').css('display', 'none');
         $('.modal-trigger').leanModal({
             dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .50, // Opacity of modal background
+            opacity: 0.50, // Opacity of modal background
             in_duration: 300, // Transition in duration
             out_duration: 200, // Transition out duration
-            complete: function() { 
+            complete: function() {
                 $('#materialize-lean-overlay-2').css('display', 'none');
-                usuarioactual = $('#nombre').val()+' '+$('#apellidos').val();
-                $.get(`/user/${usuarioactual}`, {}, (data) => {
+                //usuarioactual = $('#nombre').val()+' '+$('#apellidos').val();
+                usuarioactual = `${$('#nombre').val()} ${$('#apellidos').val()}`;
+                $.get(`/user/${usuarioactual}`, {}, () => {
                     actualizar();
-                    actualizar_usuarios();
+                    actualizarUsuarios();
                     $('#texto_bienvenida').html(`<p>Bienvenido ${usuarioactual}</p>`);
                 });
-                
+
             } // Callback for Modal close
         });
 
@@ -113,26 +113,33 @@
             }
         };
 
-        $("#boton_guardar").click(() => {
-            let nombrefichero = prompt ("Introduzca el nombre de su fichero", "Texto 1");
-            console.log("Vamos a hacer la petición post");
-            
-            
+        $('#boton_guardar').click(() => {
+            let nombrefichero = prompt('Introduzca el nombre de su fichero', 'Texto 1');
+            console.log('Vamos a hacer la petición post');
+
             $.post('/csv', {
-	            filename: nombrefichero,
-	            data: $('#original').val(),
+                filename: nombrefichero,
+                data: $('#original').val(),
                 username: `${usuarioactual}`
-            }, (res) => {console.log(res); actualizar()} , 'text').fail((err) => {if(err.status==400)alert("Ya existe un fichero con el mismo nombre en la base de datos. Introduzca otro.")});
-    
+            }, (res) => {
+                console.log(res);
+                actualizar();
+            } , 'text').fail((err) => {
+                if (err.status === 400) {
+                    alert('Ya existe un fichero con el mismo nombre en la base de datos. Introduzca otro.');
+                }
+            });
         });
 
-        const archivosenbd =`
-        <a class='dropdown-button btn waves-effect'href='#' data-activates='dropdown1'>Ficheros contenidos en la base de datos</a>
+        const archivosenbd = `
+        <a class='dropdown-button btn waves-effect'href='#' data-activates='dropdown1'>
+            Ficheros contenidos en la base de datos
+        </a>
         <ul id='dropdown1' class='dropdown-content'>
             <% archivosbd.forEach((item, i) =>{ %>
                 <li class="listabd"><a><%= item.filename %></a></li>
             <% }); %>
-        </ul>`
+        </ul>`;
 
         const actualizar = () => {
             $.get(`/user/${usuarioactual}`, {}, (archivosbd) => {
@@ -142,69 +149,68 @@
                     inDuration: 300,
                     outDuration: 225,
                     gutter: 0, // Spacing from edge
-                    belowOrigin: false, // Displays dropdown below the button
+                    belowOrigin: false // Displays dropdown below the button
                 });
                 $('li.listabd').each((_,y) => {
                     $(y).click(() => {
                         $.get(`/csv/${$(y).text()}`, {username: `${usuarioactual}`}, (data) => {
                             $('#original').val(data.data);
                         });
-                        
                     });
                 });
             });
         };
-        
+
         $('#boton_inicio_sesion').click(() => {
-            
-            if (usuarioactual == undefined)
-                alert("Seleccione o cree algún usuario");
-            else{
+            if (usuarioactual === undefined) {
+                alert('Seleccione o cree algún usuario');
+            } else {
                 $('#pag_completa').fadeIn(1600);
                 $('#pag_completa').css('display','initial');
                 $('#ventana_inicial').fadeOut(800);
             }
         });
-        
-        $("#boton_registro").click(() => {
+
+        $('#boton_registro').click(() => {
             $('#materialize-lean-overlay-2').css('display', 'block');
         });
-        
+
         $('#boton_salir').click(() => {
             usuarioactual = undefined;
             $('#texto_bienvenida').empty();
             $('#pag_completa').fadeOut(1600);
             $('#pag_completa').css('display','initial');
             $('#ventana_inicial').fadeIn(800);
-            $('#original').val("");
+            $('#original').val('');
         });
 
         const handleDragLeave = () => {
             $('#drag_and_drop').css('background-color', '#cfd8dc');
             $('#drag_and_drop').css('animation', 'none');
         };
-        
-        const usuariosenbd =`
-        <a class='dropdown-button btn boton_usuarios waves-effect' href='#' data-activates='dropdown2'>Seleccione un usuario</a>
+
+        const usuariosenbd = `
+        <a class='dropdown-button btn boton_usuarios waves-effect' href='#' data-activates='dropdown2'>
+            Seleccione un usuario
+        </a>
           <ul id='dropdown2' class='dropdown-content'>
             <% usuariosbd.forEach((item, i) =>{ %>
                 <li class="listausuarios"><a><%= item %></a></li>
             <% }); %>
-        </ul>`
-        
-        const actualizar_usuarios = () => {
+        </ul>`;
+
+        const actualizarUsuarios = () => {
             $.get('/user/*', {}, (usuariosbd) => {
-                
                 let template = _.template(usuariosenbd)({usuariosbd});
                 $('#ventana_inicio_sesion_2').html(template);
-                
+
                 $('.dropdown-button').dropdown({
                     inDuration: 300,
                     outDuration: 225,
                     gutter: 0, // Spacing from edge
-                    belowOrigin: false, // Displays dropdown below the button
+                    belowOrigin: false // Displays dropdown below the button
                 });
-                
+
                 $('li.listausuarios').each((_,y) => {
                     $(y).click(() => {
                         $('#texto_bienvenida').html(`<p>Bienvenido ${$(y).text()}</p>`);
@@ -214,15 +220,13 @@
                 });
             });
         };
-        
-        actualizar_usuarios();
-            
+
+        actualizarUsuarios();
 
         $('#icono_nube_verde').bind('dragover', handleDragOver);
         $('#icono_nube_verde').bind('drop', handleFileSelect);
         $('#icono_nube_verde').bind('dragleave', handleDragLeave);
         $('#boton_subir_fichero').bind('change', handleFileSelect);
-
     });
 
     exports.main = this.main;
